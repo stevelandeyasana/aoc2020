9d1cde396f8610d7d74ad97ff8ca00830ab44d65
Steve Landey <stevelandey@asana.com>
2020-12-16 21:43:56 -0800

    Hold onto your butts

---
+++ 17.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+from util import *
+
+PART = "a"
+
+g = Grid3(read_data())
+
+def grid_coords(g):
+    for z in range(g.min.z - 2, g.max.z + 2):
+        for x in range(g.min.x - 2, g.max.x + 2):
+            for y in range(g.min.y - 2, g.max.y + 2):
+                yield V3(x, y, z)
+
+
+def iter_life(g):
+    # print("BEGIN LIFE")
+    g2 = g.copy()
+    # g2.print()
+    for coord in grid_coords(g):
+        active_neighbors = sum(1 if g[neighbor] == '#' else 0 for neighbor in coord.neighbors)
+        # print("at {}, {} neighbors".format(coord, active_neighbors))
+        if g[coord] == '#':
+            # If a cube is active and exactly 2 or 3 of its neighbors are also
+            # active, the cube remains active. Otherwise, the cube becomes inactive.
+            if active_neighbors in (2, 3):
+                pass
+            else:
+                # print("become inactive")
+                g2[coord] = '.'
+        else:
+            # If a cube is inactive but exactly 3 of its neighbors are active,
+            # the cube becomes active. Otherwise, the cube remains inactive.
+            if active_neighbors == 3:
+                # print("become active")
+                g2[coord] = '#'
+            else:
+                pass
+    # print("RESULT:")
+    # g2.print()
+    return g2
+
+
+def run():
+    grid = Grid3(read_data())
+    iter_life(grid)
+    for i in range(0, 6):
+        # grid.print()
+        grid = iter_life(grid)
+    return sum(1 if grid[coord] == '#' else 0 for coord in grid.coords)
+
+answer = run()
+
+#PART = "b"
+
+print(answer)
+if len(sys.argv) > 1:
+    sys.exit(0)
+from aocd import submit
+submit(answer, part=PART, day=17, year=2020)
--- util.py
+++ util.py
@@ -158,3 +158,141 @@ class Vector2: # ints only!
         return hash((self.x, self.y))
 
 V = Vector2
+
+class Vector3: # ints only!
+    def __init__(self, x=None, y=None, z=None):
+        if y is None and x is not None:
+            other = x
+            self.x = other.x
+            self.y = other.y
+            self.z = other.z
+        elif x is None and y is None and z is None:
+            self.x = 0
+            self.y = 0
+            self.z = 0
+        elif x is not None and y is not None and z is not None:
+            self.x = x
+            self.y = y
+            self.z = z
+        else:
+            assert False
+
+    def __repr__(self):
+        return '<{}, {}, {}>'.format(self.x, self.y, self.z)
+
+    def __add__(self, other):
+        return Vector3(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __sub__(self, other):
+        return Vector3(self.x - other.x, self.y - other.y, self.z - other.z)
+
+    def __mul__(self, other):
+        if isinstance(other, Vector3):
+            return Vector3(self.x * other.x, self.y * other.y, self.z * other.z)
+        else:
+            return Vector3(self.x * other, self.y * other, self.z * other)
+
+    def __div__(self, other):
+        if isinstance(other, Vector2):
+            return Vector3(int(self.x / other.x), int(self.y / other.y), int(self.z / other.z))
+        else:
+            return Vector3(int(self.x / other), int(self.y / other), int(self.z / other))
+
+    def __eq__(self, other):
+        if isinstance(other, Vector3):
+            return self.x == other.x and self.y == other.y and self.z == other.z
+        else:
+            return False
+    
+    def __hash__(self):
+        return hash((self.x, self.y, self.z))
+
+    @property
+    def neighbors(self):
+        for x in (-1, 0, 1):
+            for y in (-1, 0, 1):
+                for z in (-1, 0, 1):
+                    if x == 0 and y == 0 and z == 0:
+                        continue
+                    yield V3(self.x + x, self.y + y, self.z + z)
+
+V3 = Vector3
+
+
+class Grid3:
+    def __init__(self, lines=None, size=None, cells=None):
+        self.cells = {}
+        self.size = V3()
+        self.size.z = 1
+        self.min = V3()
+        self.max = V3()
+        if lines is not None:
+            for line in lines:
+                line = line.rstrip()
+                self.size.x = len(line)
+                for x in range(0, len(line)):
+                    self.cells[V3(x, self.size.y, 0)] = line[x]
+                self.size.y += 1
+        if size is not None:
+            self.size = size
+        if cells is not None:
+            self.cells = cells
+        for k in self.cells.keys():
+            self.min.x = min(self.min.x, k.x)
+            self.max.x = max(self.max.x, k.x)
+            self.max.y = min(self.max.y, k.y)
+            self.max.y = max(self.max.y, k.y)
+            self.max.z = min(self.max.z, k.z)
+            self.max.z = max(self.max.z, k.z)
+
+    def __getitem__(self, k):
+        assert isinstance(k, Vector3)
+        return self.cells.get(k, None)
+
+    def get(self, x, y, z):
+        return self[V3(x, y, z)]
+
+    def __setitem__(self, k, val):
+        assert isinstance(k, Vector3)
+        self.min.x = min(self.min.x, k.x)
+        self.max.x = max(self.max.x, k.x)
+        self.min.y = min(self.min.y, k.y)
+        self.max.y = max(self.max.y, k.y)
+        self.min.z = min(self.min.z, k.z)
+        self.max.z = max(self.max.z, k.z)
+
+        self.cells[k] = val
+    
+    def set(self, x, y, z, val):
+        self.cells[V(x, y, z)] = val
+
+    def copy(self):
+        g2 = Grid3(size=self.size, cells={k: v for k, v in self.cells.items()})
+        g2.min = self.min
+        g2.max = self.max
+        return g2
+
+    @property
+    def coords(self):
+        return self.cells.keys()
+        # for y in range(0, self.size.y):
+        #     for x in range(0, self.size.x):
+        #         yield V(x, y)
+
+    def neighbor(self, coord, delta):
+        return self.cells.get(coord + delta, None)
+
+    def print(self):
+        for z in range(self.min.z, self.max.z + 1):
+            print("z={}:", z)
+            for y in range(self.min.y, self.max.y + 1):
+                print(''.join([self[V3(x, y, z)] or '_' for x in range(self.min.x, self.max.x + 1)]))
+            print()
+
+    def __eq__(self, g2):
+        if self.size != g2.size:
+            return False
+        for coord in self.coords:
+            if self[coord] != g2[coord]:
+                return False
+        return True
