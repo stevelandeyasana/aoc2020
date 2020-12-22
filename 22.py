777bf9e83a5593a2c753e51b1710b0ad0c420581
Steve Landey <stevelandey@asana.com>
2020-12-21 21:38:57 -0800

    p2

--- 22.py
+++ 22.py
@@ -40,14 +40,75 @@ def run(path):
             return result
 
 
-assert run('22ex.txt') == 306
-answer = run('22.txt')
+# assert run('22ex.txt') == 306
+# answer = run('22.txt')
+
+
+def compute_result(decks):
+    for d, deck in enumerate(decks):
+        if deck:
+            print(deck)
+            result = 0
+            for i, card in enumerate(reversed(deck)):
+                print(card, i+1)
+                result += card * (i + 1)
+            print("winner:", d, result)
+            return d, result
+
+def run2(decks):
+    print('=======')
+    print('decks:')
+    print(decks[0])
+    print(decks[1])
+    decks = [list(deck) for deck in decks]
+    seen_pairs = set()
+    n = 0
+    while decks[0] and decks[1]:
+        n += 1
+        print("----", n)
+        print(decks[0])
+        print(decks[1])
+
+        key = '|'.join([','.join([str(c) for c in deck]) for deck in decks])
+        if key in seen_pairs:
+            print('seen', key, 'before')
+            return compute_result([[a] + decks[0], []])
+        seen_pairs.add(key)
+
+        a = decks[0].pop(0)
+        b = decks[1].pop(0)
+        print(a, 'vs', b)
+        if (a, b) in seen_pairs:
+            print('seen', (a, b), 'before')
+            return compute_result([[a] + decks[0], []])
+
+        if a <= len(decks[0]) and b <= len(decks[1]):
+            print("recurse, recurse!")
+            winner, score = run2([decks[0][0:a], decks[1][0:b]])
+            if winner == 0:
+                decks[0].extend([a, b])
+            else:
+                decks[1].extend([b, a])
+        elif a > b:
+            print('1 win')
+            decks[0].extend([a, b])
+        elif a < b:
+            print('2 win')
+            decks[1].extend([b, a])
+    return compute_result(decks)
+
+print(run2(parse('22ex2.txt')))
+sys.exit(0)
+assert run2(parse('22ex.txt'))[1] == 291
+answer = run2(parse('22.txt'))
+print(answer)
+answer = answer[1]
 assert isinstance(answer, int)
 assert answer != None
 assert answer != 0
 assert answer != ''
 
-# PART = "b"
+PART = "b"
 
 print(answer)
 if len(sys.argv) > 1:
